40079a7a23cb69d3b6ac8ecbd8000b0735e693b0
Kenji Wilkins <kenji.wilkins@gmail.com>
2019-01-03 17:12:57 +1000

    struggling

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use('/post', postRouter);
 
 app.get('/', (async (req, res, next) => {
     try{
-        await mongoose.connect("mongodb://heroku_plx5fpfg:njmdagh7d6kfllr4kcd0pvafih@ds247674.mlab.com:47674/heroku_plx5fpfg",{useNewUrlParser: true, dbName:'posts'});
+        await mongoose.connect("mongodb://KenjiWilkins:Haruki1984@ds247674.mlab.com:47674/posts",{useNewUrlParser: true});
         const db = await mongoose.connection;
         const Schema = mongoose.Schema;
         const blogSchema = new Schema({
