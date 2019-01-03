46d5573fc93cf24eb936e732feca6a0269fe3e26
Kenji Wilkins <kenji.wilkins@gmail.com>
2019-01-03 11:31:57 +1000

    trying to add mlab env

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const path = require('path');
-require('dotenv').config({});
-const uri = process.env.MONGODB_URI;
-console.log(process.env.MONGODB_URI);
+// require('dotenv').config({});
+// const uri = process.env.MONGODB_URI;
+// console.log(process.env.MONGODB_URI);
 const wrap = require('./src/middleware/wrap');
 const moment = require('moment');
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.HEROKU_DB_URI);
 //mongoose.connect(uri, {useNewUrlParser: true, dbName: 'posts'});
 const db = mongoose.connection;
 const Schema = mongoose.Schema;
