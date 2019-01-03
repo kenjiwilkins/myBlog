7ce4c55727021376e3c5206023684a7c9fad3032
Kenji Wilkins <kenji.wilkins@gmail.com>
2019-01-03 17:24:26 +1000

    struggling

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,35 +2,35 @@ const express = require('express');
 const {MongoClient} = require('mongodb');
 const postRouter = express.Router();
 require('dotenv').config();
-const uri = process.env.DB_URI;
+//const uri = process.env.DB_URI;
 const moment = require('moment');
 const mongoose = require('mongoose');
 const nav = require('../dbs/nav');
 
-    // mongoose.connect(uri, {useNewUrlParser: true, dbName: 'posts'});
-    // const db = mongoose.connection;
-    // const Schema = mongoose.Schema;
-    // const blogSchema = new Schema({
-    //     title: String,
-    //     date: Date,
-    //     author: String,
-    //     postBody: String,
-    //     category: String
-    // });
+    mongoose.connect(process.env.HEROKU_DB_URI, {useNewUrlParser: true});
+    const db = mongoose.connection;
+    const Schema = mongoose.Schema;
+    const blogSchema = new Schema({
+        title: String,
+        date: Date,
+        author: String,
+        postBody: String,
+        category: String
+    });
 
 postRouter.route('/:id')
 .get( async (req, res, next) =>{
     try{
-        await mongoose.connect("mongodb://heroku_plx5fpfg:njmdagh7d6kfllr4kcd0pvafih@ds247674.mlab.com:47674/heroku_plx5fpfg",{useNewUrlParser:true});
-        const db = await mongoose.connection;
-        const Schema = mongoose.Schema;
-        const blogSchema = new Schema({
-            title: String,
-            date: Date,
-            author: String,
-            postBody: String,
-            category: String
-        });
+        
+        // const db = await mongoose.connection;
+        // const Schema = mongoose.Schema;
+        // const blogSchema = new Schema({
+        //     title: String,
+        //     date: Date,
+        //     author: String,
+        //     postBody: String,
+        //     category: String
+        // });
 
         let post = await db.model('posts', blogSchema, 'blogPosts')
         .findOne({_id : req.params.id}).exec();
