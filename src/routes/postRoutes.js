b31870d94988409591b09934871884b26ca30649
Kenji Wilkins <kenji.wilkins@gmail.com>
2019-01-03 13:10:03 +1000

    trying to add mlab env

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const uri = process.env.DB_URI;
 const moment = require('moment');
 const mongoose = require('mongoose');
-const nav = require('../js/nav');
+const nav = require('../dbs/nav');
 
     mongoose.connect(uri, {useNewUrlParser: true, dbName: 'posts'});
     const db = mongoose.connection;
