b06a7140649216cb406ecf72eea2b669436807e4
Kenji Wilkins <kenji.wilkins@gmail.com>
2019-01-03 13:49:47 +1000

    trying to add mlab env

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -7,20 +7,31 @@ const moment = require('moment');
 const mongoose = require('mongoose');
 const nav = require('../dbs/nav');
 
-    mongoose.connect(uri, {useNewUrlParser: true, dbName: 'posts'});
-    const db = mongoose.connection;
-    const Schema = mongoose.Schema;
-    const blogSchema = new Schema({
-        title: String,
-        date: Date,
-        author: String,
-        postBody: String,
-        category: String
-    });
+    // mongoose.connect(uri, {useNewUrlParser: true, dbName: 'posts'});
+    // const db = mongoose.connection;
+    // const Schema = mongoose.Schema;
+    // const blogSchema = new Schema({
+    //     title: String,
+    //     date: Date,
+    //     author: String,
+    //     postBody: String,
+    //     category: String
+    // });
 
 postRouter.route('/:id')
 .get( async (req, res, next) =>{
     try{
+        await mongoose.connect("mongodb://heroku_plx5fpfg:njmdagh7d6kfllr4kcd0pvafih@ds247674.mlab.com:47674/heroku_plx5fpfg",{useNewUrlParser:true});
+        const db = await mongoose.connection;
+        const Schema = mongoose.Schema;
+        const blogSchema = new Schema({
+            title: String,
+            date: Date,
+            author: String,
+            postBody: String,
+            category: String
+        });
+
         let post = await db.model('posts', blogSchema, 'blogPosts')
         .findOne({_id : req.params.id}).exec();
         let date = await moment(post.date).format("MMMM Do YYYY");
