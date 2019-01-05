20d684ebcf7b339bf18de12206e50be255a832a2
Kenji Wilkins <kenji.wilkins@gmail.com>
2019-01-05 11:05:49 +1000

    adding comments function

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const moment = require('moment');
 const mongoose = require('mongoose');
 const nav = require('../dbs/nav');
+const bodyParser = require('body-parser');
 
     mongoose.connect(process.env.HEROKU_DB_URI, {useNewUrlParser: true});
     const db = mongoose.connection;
@@ -16,27 +17,55 @@ const nav = require('../dbs/nav');
         postBody: String,
         category: String
     });
+    const commentSchema = new Schema({
+        name: String,
+        body: String,
+        postid: String,
+        date: Date
+    });
+
+postRouter.use(bodyParser.urlencoded({extended:false}));
+postRouter.use(bodyParser.json());
 
 postRouter.route('/:id')
 .get( async (req, res, next) =>{
     try{
-
+        let id = req.params.id;
         let post = await db.model('posts', blogSchema, 'blogPosts')
         .findOne({_id : req.params.id}).exec();
         let date = await moment(post.date).format("MMMM Do YYYY");
+        let comment = await db.model('posts', commentSchema, 'comments')
+        .find({postid : req.params.id}).exec();
+        if(!comment){
+            comments = [];
+        }
         res.render('post',{
             nav,
             title: "Ekubo mo Abata",
             page_heading: "えくぼもあばた",
             sub_heading: "by Kenji Wilkins",
             post,
-            date
+            date,
+            comments,
+            id
         });
     }
     catch(err) {
         next(err);
     }
 });
-
+postRouter.route('/send')
+    .post( async(req, res, next) =>{
+        try{
+            let comment = await db.model('posts', commentSchema, 'comments');
+            await comment.create({ name: req.body.commenterName, body: req.body.commentBody, postid: req.body.postId }, (err) =>{
+                if(err) console.log(err);
+            });
+            return Promise.resolve(res.redirect('/post/' + req.body.postId));
+        }
+        catch(err){
+            next(err);
+        }
+    })
 
 module.exports = postRouter;
